66e97c8ce5d1dd8e700bcb4b9d10e4d1513610f4
Pierre Segerström <pise@kth.se>
2026-01-22 17:13:59 +0100

    feat: prepare Main for new structure. #28

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.services.*;
+
 public class Main {
     public static void main(String[] args) {
+        // TODO: Initialize all "global variables" and pass to LaunchDecider
+        // LaunchDecider launchDecider = new LaunchDecider(null, null, null);
+        // launchDecider.decide(null, null, null, null);
         System.out.println("Hello, World!");
     }
 }
