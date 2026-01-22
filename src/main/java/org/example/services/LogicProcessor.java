4359b4070de3794a2ab21862041a6b09a6008658
Pierre Segerström <pise@kth.se>
2026-01-22 17:17:58 +0100

    feat: setup LogicProcessor. #28

---
+++ src/main/java/org/example/services/LogicProcessor.java
@@ -0,0 +1,14 @@
+package org.example.services;
+
+import org.example.model.Connector;
+
+public class LogicProcessor {
+    public boolean[][] buildPUM(boolean[] cmv, Connector[][] lcm) {
+        final int N = 15;
+        boolean[][] pum = new boolean[N][N];
+
+        // TODO: Iterate logic
+
+        return pum;
+    }
+}
