5986e12e0acbe57ef69f1a6251a179a9a631fa62
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2026-01-24 07:18:48 +0100

    feat: Implement Launch Interceptor Condition nr. 1 #3 (#31)
    * Implement LIC1
    
    * Fix distance function
    
    * Add test for condition 1

--- src/main/java/org/example/model/Point.java
+++ src/main/java/org/example/model/Point.java
@@ -1,6 +1,14 @@
 package org.example.model;
 
+import java.lang.Math;
+
 /**
  * Represents a geographical coordinate
  */
-public record Point(double x, double y){}
+public record Point(double x, double y){
+    public static double distance(Point p1, Point p2) {
+        double deltaX = p1.x() - p2.x();
+        double deltaY = p1.y() - p2.y();
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
--- src/test/java/org/example/services/ConditionCheckerTest.java
+++ src/test/java/org/example/services/ConditionCheckerTest.java
@@ -2,9 +2,12 @@ package org.example.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.example.model.Point;
 
 class ConditionCheckerTest {
+    ConditionChecker checker = new ConditionChecker();
+
     /**
      * CONTRACT: ...
      */
@@ -16,5 +19,22 @@ class ConditionCheckerTest {
 
         // Act & Assert
         assertTrue(b);
+    }
+
+    @Test
+    @DisplayName("Test condition 1")
+    void testCondition1() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(1, 1),
+            new Point(-1, -1),
+            new Point(0, 3), // Not within radius=2 of previous 3
+        };
+
+        // Test that last triplet is counted as not fitting within radius
+        assertTrue(checker.checkCondition1(points, 2));
+
+        // Big circle, no triplet that's not encompassed by this radius
+        assertFalse(checker.checkCondition1(points, 10));
     }
 }
