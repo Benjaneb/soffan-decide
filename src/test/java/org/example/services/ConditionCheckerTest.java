87e258ef21d517ed3f09502aeb7575fd2d29eb0f
Benjaneb <benjaneb@proton.me>
2026-01-24 01:25:26 +0100

    Add tests for condition 2

--- src/test/java/org/example/services/ConditionCheckerTest.java
+++ src/test/java/org/example/services/ConditionCheckerTest.java
@@ -2,9 +2,11 @@ package org.example.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.example.model.Point;
 
 class ConditionCheckerTest {
+    ConditionChecker checker = new ConditionChecker();
     /**
      * CONTRACT: ...
      */
@@ -16,5 +18,32 @@ class ConditionCheckerTest {
 
         // Act & Assert
         assertTrue(b);
+    }
+
+    @Test
+    @DisplayName("Test condition 2 with succeeding data")
+    void testCondition2Succeeds() {
+        Point[] points = {
+            new Point(2, 0), // Not correct angle with next 2
+            new Point(1, 0),
+            new Point(0, 0),
+            new Point(-1, -1),
+        };
+
+        // Test that last triplet is counted as forming an angle of PI +- 0.01
+        assertTrue(checker.checkCondition2(points, 0.01));
+    }
+
+    @Test
+    @DisplayName("Test condition 2 with no points that can form good angle")
+    void testCondition2Fails() {
+        Point[] points = {
+            new Point(2, 0),
+            new Point(1, 0),
+            new Point(0, 0),
+            new Point(-2, 0),
+        };
+
+        assertFalse(checker.checkCondition2(points, 0.01));
     }
 }
