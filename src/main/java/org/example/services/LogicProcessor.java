7f5e1cd7a3975460e47a7094747a7a74fc51ec47
Daglas Aitsen <91371101+d4gl4s@users.noreply.github.com>
2026-01-28 23:19:05 +0100

    feat: Implement PUM matrix calculation. #18 (#58)

--- src/main/java/org/example/services/LogicProcessor.java
+++ src/main/java/org/example/services/LogicProcessor.java
@@ -7,7 +7,24 @@ public class LogicProcessor {
         final int N = 15;
         boolean[][] preliminaryUnlockingsMat = new boolean[N][N];
 
-        // TODO: Iterate logic
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Connector connector = connectors[i][j];
+                switch (connector) {
+                    case NOTUSED:
+                        preliminaryUnlockingsMat[i][j] = true;
+                        break;
+                    case ANDD:
+                        preliminaryUnlockingsMat[i][j] = conditionsMet[i] && conditionsMet[j];
+                        break;
+                    case ORR:
+                        preliminaryUnlockingsMat[i][j] = conditionsMet[i] || conditionsMet[j];
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown: " + connector);
+                }
+            }
+        }
 
         return preliminaryUnlockingsMat;
     }
