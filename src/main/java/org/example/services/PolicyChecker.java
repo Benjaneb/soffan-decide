90d078c65d4590d5b3b60ee9a9981ace7389a5dc
Benjaneb <benjaneb@proton.me>
2026-01-29 01:50:53 +0100

    Implement creation of final unlocking vector

--- src/main/java/org/example/services/PolicyChecker.java
+++ src/main/java/org/example/services/PolicyChecker.java
@@ -5,8 +5,12 @@ public class PolicyChecker {
         int N = preliminaryUnlockingsVec.length; // expect 15
         boolean[] finalUnlockings = new boolean[N];
 
-        // TODO: Iterate logic
-
+        for (int i = 0; i < N; i++) {
+            boolean rowTrue = true;
+            for (boolean cell : preliminaryUnlockingsMat[i])
+                rowTrue &= cell;
+            finalUnlockings[i] = !preliminaryUnlockingsVec[i] || rowTrue;
+        }
         return finalUnlockings;
     }
 }
